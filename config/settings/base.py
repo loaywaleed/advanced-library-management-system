42dc1d30186b29ad917f68bab07fc65f8968169f
Loay Waleed <loaywaleed9@gmail.com>
2025-04-28 23:18:27 +0300

    feat: added pagination

--- config/settings/base.py
+++ config/settings/base.py
@@ -59,6 +59,8 @@ REST_FRAMEWORK = {
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 
 REST_AUTH = {
